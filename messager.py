733bc5c60a0aa95f709eae663944fdfd4e66dbce
ClaireChilders <97919301+ClaireChilders@users.noreply.github.com>
2025-06-06 17:02:00 -0400

    feat: move messager.py to package

--- messager.py
+++
@@ -1,7 +0,0 @@
-import json
-from datetime import datetime
-
-def send_message(env: str, cfg_file: str) -> str:
-    with open(cfg_file, 'r') as file:
-        config = json.load(file)
-    return f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - [{env}] - [{config['message']}]"
