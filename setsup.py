877071430ca0a01aa5ead6a49c1eaf2928bc0eee
ClaireChilders <97919301+ClaireChilders@users.noreply.github.com>
2025-06-06 16:56:03 -0400

    feat: add setsup.py

---
+++ setsup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+from messager import __version__
+
+setup(
+  name="messager",
+  version=__version__,
+  url='https://github.com/CLDC-OU/ExampleMessager',
+  author_email='clairechilders@oakland.edu'
+  py_modules=['messager']
+)
